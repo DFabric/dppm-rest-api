e7cfb79034a6e293db0d812ba0ce9fed707ef19f
D. Scott Boggs <scott@tams.tech>
2019-05-25 07:32:27 -0400

    Add route which cleans unused built packages
    moved to separate method to keep ameba from taking a long time
    
    also because it looked bad to have it so deeply nested

--- src/actions/pkg.cr
+++ src/actions/pkg.cr
@@ -1,11 +1,26 @@
 module DppmRestApi::Actions::Pkg
   extend self
+  ALL_PKGS = ""
+  ONE_PKG  = "/:id"
+
+  def output_config_for(package, to builder)
+    builder.field package.package do
+      builder.object do
+        package.pkg_file.config_vars.try &.each_key do |key|
+          builder.field key, package.get_config key
+        end
+      end
+    end
+  end
+
   # List built packages
   relative_get nil do |context|
     if context.current_user? && Actions.has_access? context, Access::Read
       pfx = Prefix.new prefix
+      # Build JSON directly to the HTTP response IO
       JSON.build context.response do |json|
         json.array do
+          # build an array of all the package names
           pfx.each_pkg { |pkg| json.string pkg.package }
         end
       end
@@ -16,7 +31,22 @@ module DppmRestApi::Actions::Pkg
   # Clean unused built packages
   relative_delete "/clean" do |context|
     if context.current_user? && Actions.has_access? context, Access::Delete
-      # TODO: delete all unused built packages
+      res = Prefix.new(prefix).clean_unused_packages(confirmation: false) { }
+      if result = res
+        if result.empty?
+          context.response.status_code = 404
+          {
+            errors: [
+              "no packages to clean",
+              "received empty set from Prefix#clean_unused_packages; please report this strange bug",
+            ],
+          }
+        else
+          result
+        end
+      else
+        {errors: ["no packages to clean"]}
+      end.to_json context.response
       next context
     end
     raise Unauthorized.new context
@@ -24,7 +54,62 @@ module DppmRestApi::Actions::Pkg
   # Query information about a given package
   relative_get "/:id/query" do |context|
     if context.current_user? && Actions.has_access? context, Access::Read
-      # TODO: Query information about the given package
+      package_name = URI.unescape context.params.url[:id]
+      # A DPPM::Package? to store the value in while searching
+      __selected_pkg = nil
+      # Iterate over the packages to find the relevant one.
+      Prefix.new(prefix).each_pkg do |pkg|
+        if pkg.package == package_name
+          __selected_pkg = pkg
+        end
+      end
+      if selected_pkg = __selected_pkg
+        # The package whose name was the :id URL parameter was found
+        if key = context.params.query["get"]?
+          # A key was specified by the &get query parameter
+          begin
+            {
+              package_name => {
+                key => selected_pkg.get_config key,
+                # This method call  ^^ raises an untyped error if the key is not found
+              },
+              "errors" => [] of Nil,
+            }.to_json context.response
+            next context
+          rescue e : Exception
+            # catch the exception thown by the lack of a config value for the specified key
+            # otherwise, pass it on to the
+            msg = e.message || raise e
+            raise e unless msg.starts_with? "config key not found"
+            context.response.status_code = 404
+            {errors: [e.message]}.to_json context.response
+            next context
+          end
+        else
+          # No specific key was requested, respond with all of the config options
+          # for this package and all dependent packages
+          JSON.build context.response do |builder|
+            builder.object do
+              output_config_for selected_pkg, to: builder
+              if context.params.query["deps"]? && (deps = selected_pkg.pkg_file.deps)
+                builder.field "dependencies" do
+                  deps.each do |name, version|
+                    semver = SemanticVersion.parse version
+                    Prefix.new(prefix).each_pkg do |pkg|
+                      if (pkg.package == name) && (pkg.semantic_version == semver)
+                        output_config_for pkg, to: builder
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      else
+        {errors: [%[no such package "#{package_name}" found]]}.to_json context.response
+        context.response.status_code = 404
+      end
       next context
     end
     raise Unauthorized.new context
