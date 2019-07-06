910f74882dca16d80242902ab7ea265e7555a199
Scott Boggs <scott@tams.tech>
2019-07-06 13:55:51 -0400

    Add user delete route

--- src/actions/user.cr
+++ src/actions/user.cr
@@ -45,14 +45,31 @@ module DppmRestApi
       rescue e : JSON::ParseException
         raise BadRequest.new context, cause: e
       end
-      config_key, user = Config::User.create userdata.groups, userdata.name
-      DppmRestApi.permissions_config.users << user
-      DppmRestApi.permissions_config.sync_to_disk
-      build_json context.response do |resp|
-        resp.field "SuccessfullyAddedUser" do
-          user.to_json resp, except: :api_key_hash
+    end
+
+    extend self
+    include RouteHelpers
+    relative_post nil do |context|
+      if Actions.has_access? context, Access::Create
+        userdata = begin
+          if body = context.request.body
+            AddUserBody.from_json body
+          else
+            raise BadRequest.new context, "adding a user requires a request body"
+          end
+        rescue e : JSON::ParseException
+          raise BadRequest.new context, cause: e
         end
-        resp.field "AccessKey", config_key
+        config_key, user = Config::User.create userdata.groups, userdata.name
+        DppmRestApi.permissions_config.users << user
+        DppmRestApi.permissions_config.sync_to_disk
+        build_json context.response do |resp|
+          resp.field "SuccessfullyAddedUser" do
+            user.to_json resp, except: :api_key_hash
+          end
+          resp.field "AccessKey", config_key
+        end
+        next context
       end
     end
     relative_delete "/:id" do |context|
