0715162c51e8fd81de72b84524b70eb7024c8b3d
Scott Boggs <scott@tams.tech>
2019-09-22 19:20:31 -0400

    Refactor group modification routes to avoid indexing
    Using the #map! and #reject! mutation methods instead of indexing and replacing at the index also should hopefully avoid race conditions as well when processing multiple parallel requests

--- src/actions/groups.cr
+++ src/actions/groups.cr
@@ -5,6 +5,12 @@ module DppmRestApi::Actions::Groups
     end
   end
 
+  class DuplicateGroup < InternalServerError
+    def initialize(context, id, cause : Exception? = nil)
+      super context, "Two groups found with ID #{id}!", cause
+    end
+  end
+
   class InvalidAccessLevel < BadRequest
     def initialize(context : HTTP::Server::Context, cause : Exception? = nil)
       level = context.params.url["access_level"]? || context.params.query["access_level"]?
@@ -31,10 +37,10 @@ module DppmRestApi::Actions::Groups
     raise Unauthorized.new context unless Actions.has_access? context, Access::Create
     body = context.request.body
     raise BadRequest.new context, "One must specify a group to add." if body.nil?
-    group = Config::Group.from_json body
-    DppmRestApi.permissions_config.groups << group
+    DppmRestApi.permissions_config.groups << Config::Group.from_json body
     DppmRestApi.permissions_config.sync_to_disk
   end
+
   # Regardless of whether the given path exists on the given group, set the
   # group's access level on that path to the given access level.
   relative_put "/:id/route/:path/:access_level" do |context|
@@ -42,36 +48,34 @@ module DppmRestApi::Actions::Groups
     group_id_s = context.params.url["id"]
     group_id = group_id_s.to_i? || raise BadRequest.new context,
       %[Group ID #{group_id_s} was not a valid integer]
-    # find the group if it already exists in the permissions config
-    index = DppmRestApi.permissions_config
-      .groups
-      .index { |grp| grp.id == group_id }
-    raise NoSuchGroup.new context, group_id_s if index.nil?
-    old_group = DppmRestApi.permissions_config.groups[index]
+    group_already_found = false
     path = URI.unescape context.params.url["path"]
     access_level = Access.parse?(context.params.url["access_level"])
     raise InvalidAccessLevel.new context if access_level.nil?
-    name = context.params
-      .query["name"]?
-      .try { |param| URI.unescape param } || old_group.name
-    # Create the group if it doesn't already exist
-    group = Config::Group.new id: group_id, name: name,
-      permissions: old_group.permissions
-    # Create a query parameters object (since the default isn't nil, it's an
-    # empty Hash) if group doesn't already have a path with query params on
-    # it.
-    existing_params = group.permissions[path]?
-      .try &.query_parameters || {} of String => Array(String)
-    # if there were query parameters specified in the body, override
-    # existing values
-    existing_params.merge! query_params_in_body?(of: context) do |_path, existing, new_val|
-      existing + new_val
+    DppmRestApi.permissions_config.groups.map! do |group|
+      next group if group.id != group_id
+      raise DuplicateGroup.new context, group_id_s if group_already_found
+      group_already_found = true
+      # Create a query parameters object (since the default isn't nil, it's an
+      # empty Hash) if group doesn't already have a path with query params on
+      # it.
+      existing_params = group.permissions[path]?
+        .try &.query_parameters || {} of String => Array(String)
+      # if there were query parameters specified in the body, override
+      # existing values
+      existing_params.merge! query_params_in_body?(of: context) do |_path, existing, new_val|
+        existing + new_val
+      end
+      # Override the existing permissions
+      group.permissions[path] = Config::Route.new access_level, existing_params
+      # allow overriding the name on this route
+      group.name = context.params.query["name"]? || group.name
+      group
     end
-    # Override the existing permissions
-    group.permissions[path] = Config::Route.new access_level, existing_params
-    # and overwrite the current value.
-    DppmRestApi.permissions_config.groups[index] = group
+    raise NoSuchGroup.new context, group_id_s unless group_already_found
+    DppmRestApi.permissions_config.sync_to_disk
   end
+
   # Remove specific query parameters from this path/access-level for this
   # group. The query parameters may be specified via query parameters like:
   # ```
@@ -101,24 +105,32 @@ module DppmRestApi::Actions::Groups
     group_id_s = context.params.url["id"]
     group_id = group_id_s.to_i? || raise BadRequest.new context,
       %[Group ID "#{group_id_s}" was not an integer!]
-    group_idx = DppmRestApi.permissions_config.groups.index { |grp| grp.id == group_id }
-    raise NoSuchGroup.new context, group_id_s if group_idx.nil?
-    group = DppmRestApi.permissions_config.groups[group_idx]
-    route = group.permissions[path]? || raise NotFound.new context,
-      "No permissions settings found for group #{group.id} (#{group.name}) \
+    group_already_found = false
+    DppmRestApi.permissions_config.groups.map do |group|
+      next group if group.id != group_id
+      raise DuplicateGroup.new context, group_id_s if group_already_found
+      group_already_found = true
+      route = group.permissions[path]? || raise NotFound.new context,
+        "No permissions settings found for group #{group.id} (#{group.name}) \
         on the path glob #{path}."
-    next route.query_parameters.clear if context.request.query_params.empty?
-    route.query_parameters.each do |key, values|
-      next unless context.request.query_params.has_key? key
-      specified = context.request.query_params.fetch_all(key).reject &.empty?
-      if specified.empty?
-        route.query_parameters.delete key
-      else
-        route.query_parameters[key] = values - specified
+      if context.request.query_params.empty?
+        route.query_parameters.clear
+        group.permissions[path] = route
+        next group
       end
+      route.query_parameters.each do |key, values|
+        next unless context.request.query_params.has_key? key
+        specified = context.request.query_params.fetch_all(key).reject &.empty?
+        if specified.empty?
+          route.query_parameters.delete key
+        else
+          route.query_parameters[key] = values - specified
+        end
+      end
+      group.permissions[path] = route
+      group
     end
-    group.permissions[path] = route
-    DppmRestApi.permissions_config.groups[group_idx] = group
+    raise NoSuchGroup.new context, group_id_s unless group_already_found
     DppmRestApi.permissions_config.sync_to_disk
   end
 
@@ -127,13 +139,19 @@ module DppmRestApi::Actions::Groups
     group_id = (group_id_s = context.params.url["id"]).to_i?
     raise BadRequest.new context,
       %[Group ID "#{group_id_s}" was not an integer] if group_id.nil?
-    group_idx = DppmRestApi.permissions_config.groups.index { |grp| grp.id == group_id }
-    raise NoSuchGroup.new context, group_id_s if group_idx.nil?
-    group = DppmRestApi.permissions_config.groups[group_idx]
-    deleted = group.permissions.delete context.params.url["path"]
-    raise NotFound.new context,
-      "no permissions found at the specified path (possibly already \
-        deleted?)" if deleted.nil?
+    group_already_found = false
+    DppmRestApi.permissions_config.groups.map! do |group|
+      next group if group.id != group_id
+      raise DuplicateGroup.new context, group_id_s if group_already_found
+      group_already_found = true
+      group.permissions.delete(
+        context.params.url["path"]
+      ) || raise NotFound.new context, "\
+                              no permissions found at the specified path \
+                              (possibly already deleted?)"
+      group
+    end
+    raise NoSuchGroup.new context, group_id_s unless group_already_found
     DppmRestApi.permissions_config.sync_to_disk
   end
 
@@ -142,11 +160,17 @@ module DppmRestApi::Actions::Groups
     group_id = (group_id_s = context.params.url["id"]).to_i?
     raise BadRequest.new context,
       %[Group ID "#{group_id_s}" was not an integer] if group_id.nil?
-    to_be_deleted = DppmRestApi.permissions_config
+    group_already_found = false
+    DppmRestApi.permissions_config
       .groups
-      .index { |group| group.id == group_id }
-    raise NoSuchGroup.new context, group_id_s if to_be_deleted.nil?
-    DppmRestApi.permissions_config.groups.delete_at to_be_deleted
+      .reject! do |group|
+        if group.id == group_id
+          raise DuplicateGroup.new context, group_id_s if group_already_found
+          next group_already_found = true
+        end
+        false
+      end
+    raise NoSuchGroup.new context, group_id_s unless group_already_found
     DppmRestApi.permissions_config.sync_to_disk
   end
 end
